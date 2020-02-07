b0615c80539962b9f4810b4116695167f0108c32
AaronODea <c00217820@itcarlow.ie>
2020-02-07 10:25:42 +0000

    map start

---
+++ ARGO/ARGO/Player.cpp
@@ -0,0 +1,37 @@
+#include "Player.h"
+
+Player::Player()
+{
+	SetUp();
+}
+
+Player::~Player()
+{
+}
+
+void Player::SetUp()
+{
+	rect.x = 0;
+	rect.y = 0;
+	rect.w = 20;
+	rect.h = 20;
+}
+
+void Player::update()
+{
+}
+
+void Player::render(SDL_Renderer* t_renderer)
+{
+	SDL_SetRenderDrawColor(t_renderer, 255, 255, 255, 255);
+	SDL_RenderFillRect(t_renderer, &rect);
+	SDL_SetRenderDrawColor(t_renderer, 0, 0, 0, 255);
+}
+
+void Player::setPosition()
+{
+}
+
+void Player::getPosition()
+{
+}
